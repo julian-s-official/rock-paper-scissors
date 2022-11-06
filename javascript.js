2d6a3996434e78fa4ffa912bd22249f25ff650db
julian <julian_stanger@web.de>
2022-11-06 17:20:42 +0100

    Add function to randomly generate pc choice

--- javascript.js
+++ javascript.js
@@ -1,8 +1,20 @@
 /* 
 Requirements
-
+------------
 Function to randomly generate Rock, Paper or Scissors as computer choice
 Function that has player and computer selection as input and plays game
 Function Game that allows multiple rounds and outputs results
-
 */
+
+function getComputerChoice(){
+  let array = [
+    'Rock',
+    'Paper',
+    'Scissors'
+  ];
+  let randomNumber = Math.floor(Math.random()*array.length);
+
+  return array[randomNumber];
+}
+
+console.log(getComputerChoice());
