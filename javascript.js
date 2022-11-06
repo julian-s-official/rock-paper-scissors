715012f229fdba81d1f2b92a463b495bebdb3580
julian <julian_stanger@web.de>
2022-11-06 17:38:19 +0100

    Add ability to play single round

--- javascript.js
+++ javascript.js
@@ -17,4 +17,46 @@ function getComputerChoice(){
   return array[randomNumber];
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection.toLowerCase() == 'rock') {
+    switch(computerSelection.toLowerCase()) {
+      case 'rock':
+        return "It's a draw!";
+      case 'paper':
+        return "You Lose! Paper beats Rock";
+      case 'scissors':
+        return "You Win! Rock beats Scissors!";
+      default: 
+        return "Something went wrong";
+    }
+  } else if (playerSelection.toLowerCase() == 'paper') {
+    switch(computerSelection.toLowerCase()) {
+      case 'rock':
+        return "You Win! Paper beats Rock!";
+      case 'paper':
+        return "It's a draw!";
+      case 'scissors':
+        return "You Lose! Scissors beats Paper";
+      default: 
+        return "Something went wrong";
+    }
+  } else if (playerSelection.toLowerCase() == 'scissors') {
+    switch(computerSelection.toLowerCase()) {
+      case 'rock':
+        return "You Lose! Rock beats Scissors!";
+      case 'paper':
+        return "You Win! Scissors beat Paper!";
+      case 'scissors':
+        return "It's a draw!";
+      default: 
+        return "Something went wrong";
+    }
+  } else {
+    return "Didn't select Rock, Paper or Scissors!"
+  }
+}
+
+const playerSelection = prompt("Make your choice!")
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
