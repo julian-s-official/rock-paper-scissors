a4451a48f37eb52b40e1195f974263db2ab35fee
julian <julian_stanger@web.de>
2022-11-06 17:16:41 +0100

    Update requirements as comment

---
+++ javascript.js
@@ -0,0 +1,8 @@
+/* 
+Requirements
+
+Function to randomly generate Rock, Paper or Scissors as computer choice
+Function that has player and computer selection as input and plays game
+Function Game that allows multiple rounds and outputs results
+
+*/
